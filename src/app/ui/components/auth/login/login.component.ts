834c8f5311151dd4ccd97fcade23ce8f44cc00e2
batuhan topcu <topcubatuhan00@gmail.com>
2023-05-02 15:27:06 +0300

    generic button added

--- src/app/ui/components/auth/login/login.component.ts
+++ src/app/ui/components/auth/login/login.component.ts
@@ -2,19 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
-import { ValidInputDirective } from 'src/app/common/valid-input.directive';
+import { ValidInputDirective } from 'src/app/common/directives/valid-input.directive';
+import { LoadingButtonComponent } from 'src/app/common/components/loading-button/loading-button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterModule, FormsModule, ValidInputDirective],
+  imports: [CommonModule,RouterModule, FormsModule, ValidInputDirective, LoadingButtonComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  isLoading: boolean = false;
+
+
   login(form:NgForm){
       if(form.valid){
+        this.isLoading = true;
         console.log(form.value)
+        setTimeout(() => {
+          this.isLoading = false;
+        },3000)
       }
   }
 }
